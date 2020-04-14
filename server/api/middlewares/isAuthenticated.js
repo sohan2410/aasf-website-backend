315bb9e2e0e9058e8506a020107c50ec848abc43
Guna Shekar <gunashekherproddatoori@gmail.com>
2020-04-14 13:04:28 +0530

    change token to bearer token

--- server/api/middlewares/isAuthenticated.js
+++ server/api/middlewares/isAuthenticated.js
@@ -3,10 +3,11 @@ import { jwtSecret } from "../../common/config";
 
 // eslint-disable-next-line no-unused-vars, no-shadow
 export default async function isAuthenticated(req, res, next) {
-  const token = req.headers.authorization;
+  let token = req.headers.authorization;
   if (!token) res.status(401).send({ message: "Token missing" });
   else {
     try {
+      token = token.split(" ")[1];
       const user = jwt.verify(token, jwtSecret);
       req.user = user;
       next();
