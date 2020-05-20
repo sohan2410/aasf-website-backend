66195946ccabda8306befa0323752cc59ce12178
Guna Shekar <gunashekherproddatoori@gmail.com>
2020-05-21 03:01:48 +0530

    complete user tests

--- test/users.test.js
+++ test/users.test.js
@@ -22,32 +22,34 @@ describe("Users Authorization", () => {
       .send({ roll: "2018BCS-000", password: "aasf_iiitm" });
     expect(response.body).to.be.an("object");
     expect(response.status).to.equal(200);
-    expect(response.body).to.have.property("token");
-    expect(response.body).to.have.property("message");
+    expect(response.body).to.have.all.keys("token", "message");
+
     expect(response.body.token).to.be.a("string");
     expect(response.body.message).to.be.a("string");
   });
 
   it("should fail login when provided with wrong credentials", async () => {
+    //Incorrect roll number
     let response = await request(Server)
       .post("/users/login")
       .send({ roll: "2018BCS-00", password: "aasf_iiitm" });
     expect(response.body).to.be.an("object");
-    expect(response.status).to.be.equal(400);
+    expect(response.status).to.equal(400);
     expect(response.body).to.have.property("message");
     expect(response.body.message).to.be.a("string");
 
+    //Incorrect Password
     response = await request(Server)
       .post("/users/login")
       .send({ roll: "2018BCS-000", password: "aasf_iiit" });
     expect(response.body).to.be.an("object");
-    expect(response.status).to.be.equal(401);
+    expect(response.status).to.equal(401);
     expect(response.body).to.have.property("message");
     expect(response.body.message).to.be.a("string");
   });
 });
 
-describe("User Operations(Change Password, Get Details)", () => {
+describe("User Operations(Change Password, Get Details, Leaderboard)", () => {
   before(async () => {
     await mongo();
     await userModel.deleteMany({});
@@ -75,13 +77,14 @@ describe("User Operations(Change Password, Get Details)", () => {
     expect(response.body).to.have.property("message");
     expect(response.body.message).to.be.a("string");
 
+    //Check if user can login with new password
     response = await request(Server)
       .post("/users/login")
       .send({ roll: "2018BCS-000", password: "aasf@iiitm" });
     expect(response.body).to.be.an("object");
     expect(response.status).to.equal(200);
-    expect(response.body).to.have.property("token");
-    expect(response.body).to.have.property("message");
+    expect(response.body).to.have.all.keys("token", "message");
+
     expect(response.body.token).to.be.a("string");
     expect(response.body.message).to.be.a("string");
   });
@@ -91,7 +94,7 @@ describe("User Operations(Change Password, Get Details)", () => {
       .put("/users/password")
       .send({ currentPassword: "aasf@iiitm", newPassword: "aasf_iiitm" });
     expect(response.body).to.be.an("object");
-    expect(response.status).to.be.equal(401);
+    expect(response.status).to.equal(401);
     expect(response.body).to.have.property("message");
     expect(response.body.message).to.be.a("string");
 
@@ -100,7 +103,7 @@ describe("User Operations(Change Password, Get Details)", () => {
       .set("Authorization", `bearer dummy`)
       .send({ currentPassword: "aasf@iiitm", newPassword: "aasf_iiitm" });
     expect(response.body).to.be.an("object");
-    expect(response.status).to.be.equal(401);
+    expect(response.status).to.equal(401);
     expect(response.body).to.have.property("message");
     expect(response.body.message).to.be.a("string");
   });
@@ -111,24 +114,59 @@ describe("User Operations(Change Password, Get Details)", () => {
       .set("Authorization", `bearer ${token}`);
     expect(response.body).to.be.an("object");
     expect(response.status).to.equal(200);
-    expect(response.body).to.have.property("user");
-    expect(response.body).to.have.property("message");
+    expect(response.body).to.have.all.keys("user", "message");
+
     expect(response.body.user).to.be.an("object");
     expect(response.body.message).to.be.a("string");
 
-    expect(response.body.user).to.have.property("_id");
+    expect(response.body.user).to.include.all.keys(
+      "_id",
+      "name",
+      "score",
+      "achievements",
+      "role"
+    );
     expect(response.body.user._id).to.be.a("string");
-
-    expect(response.body.user).to.have.property("name");
     expect(response.body.user.name).to.be.a("string");
-
-    expect(response.body.user).to.have.property("score");
     expect(response.body.user.score).to.be.an("object");
-
-    expect(response.body.user).to.have.property("achievements");
     expect(response.body.user.achievements).to.be.an("object");
-
-    expect(response.body.user).to.have.property("role");
+    expect(response.body.user.achievements).to.have.all.keys(
+      "first",
+      "second",
+      "third"
+    );
     expect(response.body.user.role).to.be.a("string");
+  });
+
+  it("fetches leaderboard", async () => {
+    const response = await request(Server)
+      .get("/users/leaderboard")
+      .set("Authorization", `bearer ${token}`);
+
+    expect(response.body).to.be.an("object");
+    expect(response.status).to.equal(200);
+
+    expect(response.body).to.have.all.keys(
+      "totalScore",
+      "leaderboard",
+      "message"
+    );
+
+    expect(response.body.totalScore).to.be.an("object");
+    expect(response.body.totalScore).to.have.all.keys(
+      "technical",
+      "managerial",
+      "oratory"
+    );
+
+    expect(response.body.totalScore.technical)
+      .to.be.a("number")
+      .to.equal(0);
+    expect(response.body.totalScore.managerial)
+      .to.be.a("number")
+      .to.equal(0);
+    expect(response.body.totalScore.oratory)
+      .to.be.a("number")
+      .to.equal(0);
   });
 });
