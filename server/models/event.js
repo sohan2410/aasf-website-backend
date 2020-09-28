c37f862b8e6c217c4fb6ed1e0abdcb635f3c0fc4
Guna Shekar <gunashekherproddatoori@gmail.com>
2020-09-28 21:10:13 +0530

    add comments to models

--- server/models/event.js
+++ server/models/event.js
@@ -1,24 +1,30 @@
 import mongoose from "mongoose";
 
 const Event = new mongoose.Schema({
+  //Name of the event
   name: {
     type: String,
     required: [true, "Name required"],
   },
+  //Start Date for the event
   startDate: {
     type: String,
     required: [true, "Date required"],
   },
+  //Number of days the event is going to last
   numberOfDays: {
     type: Number,
     default: 1,
   },
+  //Category of the event
   category: {
     type: String,
     required: true,
     enum: ["technical", "oratory", "managerial", "miscellaneous"],
     lowercase: true,
   },
+  //Attendance for each day of the event, i.e., attendace[0] will have the attendance for Day 1,
+  //attendance[1] for Day 2 and so on.
   attendance: {
     type: [
       {
@@ -27,11 +33,16 @@ const Event = new mongoose.Schema({
       },
     ],
   },
+  //Each event is assigned an importance from 1-3
   importance: Number,
+  //Field to denote whether the QR Code for this event was generated or not.
+  //This is used to calculate number of completed events.
   qr: {
     type: Boolean,
     default: false,
   },
+  //Field to denote whether points were added to winners or not.
+  //This is used to calculate total points given to calculate percentage score for a user.
   winners: {
     type: Boolean,
     default: false,
--- server/models/user.js
+++ server/models/user.js
@@ -2,54 +2,60 @@ import mongoose from "mongoose";
 import { defaultPassword } from "../common/config";
 
 const User = new mongoose.Schema({
+  //Primary Key of the user will be their roll number
   _id: {
     type: String,
-    required: [true, "Roll number required"]
+    required: [true, "Roll number required"],
   },
+  //Name of the user
   name: {
     type: String,
-    required: [true, "Name required"]
+    required: [true, "Name required"],
   },
+  //Hashed password
   password: {
     type: String,
-    default: defaultPassword
+    default: defaultPassword,
   },
+  //Base-64 encoded display picture
   dp: {
-    type: String
-    //set a default dp later
+    type: String,
   },
+  //Scores in different categories
   score: {
     technical: {
       type: Number,
-      default: 0
+      default: 0,
     },
     managerial: {
       type: Number,
-      default: 0
+      default: 0,
     },
     oratory: {
       type: Number,
-      default: 0
-    }
+      default: 0,
+    },
   },
+  //Achievements in contests
   achievements: {
     first: {
       type: [String],
-      default: []
+      default: [],
     },
     second: {
       type: [String],
-      default: []
+      default: [],
     },
     third: {
       type: [String],
-      default: []
-    }
+      default: [],
+    },
   },
+  //Role of the user - user/admin. user by default
   role: {
     type: String,
-    default: "user"
-  }
+    default: "user",
+  },
 });
 
 export default mongoose.model("User", User);
