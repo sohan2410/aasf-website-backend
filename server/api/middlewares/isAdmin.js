2121a33e35a2905117a4eafbabb6aade3dd134f1
Guna Shekar <gunashekherproddatoori@gmail.com>
2020-12-16 18:22:29 +0530

    modify admin middleware

--- server/api/middlewares/isAdmin.js
+++ server/api/middlewares/isAdmin.js
@@ -1,23 +1,15 @@
-import UsersService from "../services/users.service";
+import UsersService from '../services/users.service';
 
 // eslint-disable-next-line no-unused-vars, no-shadow
 export default async function isAdmin(req, res, next) {
-  if (!req.user)
-    res
-      .status(401)
-      .send({ message: "You are not authorized to perform this action" });
+  if (!req.user) res.status(401).send({ message: 'You are not authorized to perform this action' });
   else {
     try {
-      const userDetails = await UsersService.getUserDetails(req.user.roll);
-      if (userDetails.role === "admin") next();
-      else
-        res
-          .status(401)
-          .send({ message: "You are not authorized to perform this action" });
+      const { user } = await UsersService.getUserDetails(req.user.roll);
+      if (user.role === 'admin') next();
+      else res.status(401).send({ message: 'You are not authorized to perform this action' });
     } catch (err) {
-      res
-        .status(err.status || 500)
-        .send({ message: err.message || "Some error has occurred" });
+      res.status(err.status || 500).send({ message: err.message || 'Some error has occurred' });
     }
   }
 }
