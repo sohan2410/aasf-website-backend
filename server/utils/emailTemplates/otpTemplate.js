4442c9bcad55d2e156b278120184900b50ed144e
Sohan Bandary <bandarysohan24@gmail.com>
2021-10-04 12:29:20 +0530

    changed otp template

--- server/utils/emailTemplates/otpTemplate.js
+++ server/utils/emailTemplates/otpTemplate.js
@@ -1,4 +1,5 @@
 export const otpTemplate = (name, otp) => `
 Dear ${name}, \n
-Your OTP to reset AASF account password is: ${otp}
+Your OTP to reset AASF account password is: ${otp}\n
+OTP expires after 1 hour
 `;
