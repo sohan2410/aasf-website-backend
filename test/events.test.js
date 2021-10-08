73395e10d124e18f0e83fa2e2544190ec853201d
kailash360 <kailashkejriwal21@gmail.com>
2021-10-08 18:00:50 +0530

    Minor fixes

--- test/events.test.js
+++ test/events.test.js
@@ -9,10 +9,7 @@ import eventModel from '../server/models/event';
 
 const expect = chai.expect;
 
-chai.use(require('chai-like'));
-chai.use(require('chai-things'))
-
-describe('Event Actions', function() {
+describe('Event Actions', function () {
   let token;
 
   before(async () => {
@@ -28,17 +25,17 @@ describe('Event Actions', function() {
       role: 'admin',
       password,
     });
-    
+
     //logging in the user
     const result = await request(Server)
-    .post('/users/login')
-    .send({ roll: '2018BCS-000', password: 'aasf_iiitm' });
+      .post('/users/login')
+      .send({ roll: '2018BCS-000', password: 'aasf_iiitm' });
     token = result.body.token;
   })
 
   //Create an event object before each test
   beforeEach(async () => {
-    
+
     //creatiing an event
     await eventModel.create({
       name: "webkriti",
@@ -47,23 +44,23 @@ describe('Event Actions', function() {
       category: "technical",
       importance: 0
     })
-    
+
   })
 
   afterEach(async () => {
     await eventModel.deleteMany({});
   })
-  
+
   after(async () => {
     await userModel.deleteMany({});
   });
 
   //Test for fetching events
-  it('should get events', async() => {
-  
+  it('should get events', async () => {
+
     let response = await request(Server)
-    .get('/admin/events')
-    .set('Authorization', `bearer ${token}`);
+      .get('/admin/events')
+      .set('Authorization', `bearer ${token}`);
 
     expect(response.body).to.be.an('object');
     expect(response.status).to.equal(200);
@@ -71,14 +68,14 @@ describe('Event Actions', function() {
     expect(response.body.events).to.be.an('array')
     expect(response.body.events[0].name).to.be.a('string')
     expect(response.body.events[0].startDate).to.be.a('string')
-    expect(response.body.events[0].numberOfDays).to.be.a('string')
+    expect(response.body.events[0].numberOfDays).to.be.a('number')
     expect(response.body.events[0].category).to.be.a('string')
     expect(response.body.events[0].attendance).to.be.an('array')
 
   })
 
   //Test for adding events by admin 
-  it("should allow to add events",async()=>{
+  it("should allow to add events", async () => {
 
     const event = {
       name: "webkriti",
@@ -101,24 +98,24 @@ describe('Event Actions', function() {
 
 
   //test to check for the editing of events 
-  it("should be able to edit events",async()=>{
-    
-    let event = await eventModel.findOne({name:"webkriti"})
-    
+  it("should be able to edit events", async () => {
+
+    let event = await eventModel.findOne({ name: "webkriti" })
+
     /*SUCCESS*/
     let response = await request(Server)
       .put(`/admin/events/${event._id}`)
       .set('Authorization', `bearer ${token}`)
-      .send({name:"gitkriti"});
-    
+      .send({ name: "gitkriti" });
+
     expect(response.status).to.equal(200)
     expect(response.body.message).to.be.a('string')
-    
+
     /*FAILURE*/
     response = await request(Server)
       .put(`/admin/events/615f758ed92d2b5ee1975eb8`) ///usiing a random event id
       .set('Authorization', `bearer ${token}`)
-      .send({name:"gitkriti"});
+      .send({ name: "gitkriti" });
 
     expect(response.status).to.equal(400)
     expect(response.body.message).to.be.a('string')
@@ -128,32 +125,31 @@ describe('Event Actions', function() {
 
 
   //Test to delete events 
-  it("should delete events",async()=>{
+  it("should delete events", async () => {
 
-    let event = await eventModel.findOne({name:"webkriti"})
+    let event = await eventModel.findOne({ name: "webkriti" })
 
     let response = await request(Server)
       .delete(`/admin/events/${event._id}`)
       .set('Authorization', `bearer ${token}`)
-      .send({name:"gitkriti"});
-    
+
     expect(response.status).to.equal(200)
     expect(response.body.message).to.be.a('string')
 
   })
 
-  
-  
-  //Test to get the QR code of events
-  it("should get the QR code of events",async ()=>{
 
-    let event = await eventModel.findOne({name:"webkriti"})
+
+  //Test to get the QR code of events
+  it("should get the QR code of events", async () => {
+
+    let event = await eventModel.findOne({ name: "webkriti" })
 
     /*SUCCESS*/
     let response = await request(Server)
       .get(`/admin/events/qr/${event._id}/1`)
       .set('Authorization', `bearer ${token}`);
-    
+
     expect(response.status).to.equal(200)
     expect(response.body.message).to.be.a('string')
     expect(response.body.qr).to.be.a('string')
@@ -162,87 +158,87 @@ describe('Event Actions', function() {
     response = await request(Server)
       .get(`/admin/events/qr/${event._id}/5`) //Invalid number of days
       .set('Authorization', `bearer ${token}`);
-    
+
     expect(response.status).to.equal(400)
     expect(response.body.message).to.be.a('string')
-    })
+  })
 
-  
-  
+
+
   //To add goodies to the events for the eligible students 
-  it("should allow to add goodies",async () => {
+  it("should allow to add goodies", async () => {
 
-    const event = await eventModel.findOne({name:"webkriti"})
+    const event = await eventModel.findOne({ name: "webkriti" })
 
     await userModel.create({
-      _id:"2020BCS-044",
-      name:"Kailash Kejriwal",
-      password:"123",
-      roll:"2020BCS-044"
+      _id: "2020BCS-044",
+      name: "Kailash Kejriwal",
+      password: "123",
+      roll: "2020BCS-044"
     })
 
     /*SUCCESS*/
     let response = await request(Server)
       .post(`/admin/events/goodies`)
       .set('Authorization', `bearer ${token}`)
-      .send({roll:"2020BCS-044",eventId:event._id});
+      .send({ roll: "2020BCS-044", eventId: event._id });
 
-      expect(response.status).to.equal(200)
-      expect(response.body.message).to.be.a('string')
-      
-      /*FAILURE*/
+    expect(response.status).to.equal(200)
+    expect(response.body.message).to.be.a('string')
+
+    /*FAILURE*/
     response = await request(Server)
-      .post(`/admin/events/goodies`) //Invalid number of days
+      .post(`/admin/events/goodies`)
       .set('Authorization', `bearer ${token}`)
-      .send({roll:"2020BCS-044",eventId:"615f758ed92d2b5ee1975eb8"}); //using a random id
-      
+      .send({ roll: "2020BCS-044", eventId: "615f758ed92d2b5ee1975eb8" }); //using a random id
+
     expect(response.status).to.equal(400)
     expect(response.body.message).to.be.a('string')
 
   })
 
-  
-  
-  //To add goodies to the events for the eligible students 
-  it("should allow to add winners",async () => {
 
-    const event = await eventModel.findOne({name:"webkriti"})
+
+  //To add goodies to the events for the eligible students 
+  it("should allow to add winners", async () => {
+
+    const event = await eventModel.findOne({ name: "webkriti" })
 
     //create sample users
     await userModel.create({
-      _id:"2020BCS-045",
-      name:"Winner 1",
-      password:"123",
-      roll:"2020BCS-045"
+      _id: "2020BCS-045",
+      name: "Winner 1",
+      password: "123",
+      roll: "2020BCS-045"
     })
     await userModel.create({
-      _id:"2020BCS-031",
-      name:"Winner 2",
-      password:"123",
-      roll:"2020BCS-031"
+      _id: "2020BCS-031",
+      name: "Winner 2",
+      password: "123",
+      roll: "2020BCS-031"
     })
     await userModel.create({
-      _id:"2020BCS-050",
-      name:"Winner 3",
-      password:"123",
-      roll:"2020BCS-050"
+      _id: "2020BCS-050",
+      name: "Winner 3",
+      password: "123",
+      roll: "2020BCS-050"
     })
 
     /*SUCCESS*/
     let response = await request(Server)
       .post(`/admin/events/winners`)
       .set('Authorization', `bearer ${token}`)
-      .send({winners:[["2020BCS-044"],["2020BCS-031","2020BCS-050"]],eventId:event._id});
-      
-      expect(response.status).to.equal(200)
-      expect(response.body.message).to.be.a('string')
-      
+      .send({ winners: [["2020BCS-044"], ["2020BCS-031", "2020BCS-050"]], eventId: event._id });
+
+    expect(response.status).to.equal(200)
+    expect(response.body.message).to.be.a('string')
+
     /*FAILURE*/
     response = await request(Server)
       .post(`/admin/events/winners`) //Invalid number of days
       .set('Authorization', `bearer ${token}`)
-      .send({winners:[["2020BCS-044"],["2020BCS-031","2020BCS-055"]],eventId:"615f758ed92d2b5ee1975eb8"}); //using invalid roll number and event id
-    
+      .send({ winners: [["2020BCS-044"], ["2020BCS-031", "2020BCS-055"]], eventId: "615f758ed92d2b5ee1975eb8" }); //using invalid roll number and event id
+
     expect(response.status).to.equal(400)
     expect(response.body.message).to.be.a('string')
 
